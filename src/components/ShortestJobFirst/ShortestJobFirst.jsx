734eb00a2547a236f47c98f7bcc81bff2e0a82fa
Neecs <nikosgil12@gmail.com>
2023-10-31 23:53:38 -0500

    some validations and SJF working

--- src/components/ShortestJobFirst/ShortestJobFirst.jsx
+++ src/components/ShortestJobFirst/ShortestJobFirst.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const ShortestJobFirst = () => {
   const [processes, setProcesses] = useState([
@@ -9,65 +9,103 @@ export const ShortestJobFirst = () => {
     { id: 5, name: 'Process E', arrivalTime: 0, burstTime: 2 },
     { id: 6, name: 'Process F', arrivalTime: 0, burstTime: 20 },
     { id: 7, name: 'Process G', arrivalTime: 0, burstTime: 5 },
-    // Agrega más procesos aquí
   ]);
 
   const [currentTime, setCurrentTime] = useState(0);
   const [runningProcess, setRunningProcess] = useState(null);
+  const [isSimulationRunning, setIsSimulationRunning] = useState(false);
 
-  // Función para recargar la página
+  const [description, setDescription] = useState(
+    'Shortest Job First (SJF) is a CPU scheduling algorithm that selects the process with the smallest burst time for execution.'
+  );
+
+  const handleBurstTimeChange = (processId, newBurstTime) => {
+    if (!isNaN(newBurstTime)) {
+      setProcesses((prevProcesses) =>
+        prevProcesses.map((process) =>
+          process.id === processId
+            ? { ...process, burstTime: newBurstTime }
+            : process
+        )
+      );
+    }
+  };
+
+  const startSimulation = () => {
+    if (!isSimulationRunning) {
+      setIsSimulationRunning(true);
+    }
+  };
+
   const reloadPage = () => {
     window.location.reload();
   };
 
   useEffect(() => {
-    const runSJF = () => {
-      if (processes.length === 0) return;
+    if (isSimulationRunning) {
+      const runSJF = () => {
+        // Filtra los procesos que ya han llegado
+        const arrivedProcesses = processes.filter(
+          (process) => process.arrivalTime <= currentTime
+        );
 
-      // Filtra los procesos que ya han llegado
-      const arrivedProcesses = processes.filter(process => process.arrivalTime <= currentTime);
+        if (arrivedProcesses.length === 0) {
+          return;
+        }
 
-      // Ordena los procesos por su tiempo de ráfaga (burst time)
-      arrivedProcesses.sort((a, b) => a.burstTime - b.burstTime); // Corregir la función de ordenamiento
+        arrivedProcesses.sort((a, b) => a.burstTime - b.burstTime);
 
-      if (arrivedProcesses.length === 0) {
-        setCurrentTime(currentTime + 1); // Avanzar el tiempo si no hay procesos listos
-        return;
-      }
+        const nextProcessToRun = arrivedProcesses[0];
+        setRunningProcess(nextProcessToRun);
 
-      const nextProcessToRun = arrivedProcesses[0];
-      setRunningProcess(nextProcessToRun);
+        const updatedProcesses = processes.filter(
+          (process) => process.id !== nextProcessToRun.id
+        );
+        setProcesses(updatedProcesses);
+        setCurrentTime(currentTime + nextProcessToRun.burstTime);
+      };
 
-      // Actualiza el estado de los procesos y avanza el tiempo
-      setProcesses(prevProcesses => prevProcesses.filter(process => process.id !== nextProcessToRun.id));
-      setCurrentTime(currentTime + nextProcessToRun.burstTime);
+      const simulationInterval = setInterval(runSJF, 1000);
 
-      // Simular la ejecución del proceso
-      setTimeout(() => {
-        setRunningProcess(null);
-      }, nextProcessToRun.burstTime * 1000); // Simulación de tiempo real
-    };
-
-    // Llama a la función de simulación cada segundo
-    const simulationInterval = setInterval(runSJF, 1000);
-
-    // Limpia el intervalo cuando se desmonta el componente
-    return () => clearInterval(simulationInterval);
-  }, [currentTime, processes]);
+      return () => {
+        clearInterval(simulationInterval);
+        setIsSimulationRunning(false);
+      };
+    }
+  }, [currentTime, processes, isSimulationRunning]);
 
   return (
     <div>
       <h1>Shortest Job First (SJF) Simulation</h1>
+      <button onClick={startSimulation}>Siguiente proceso</button>
       <button onClick={reloadPage}>Reiniciar simulación</button>
       <div>
         <h2>Current Time: {currentTime}</h2>
-        <h2>Running Process: {runningProcess ? runningProcess.name : 'None'}</h2>
+        <h2>
+          Running Process: {runningProcess ? runningProcess.name : 'None'}
+        </h2>
+      </div>
+      <div>
+        <h2>Description of SJF Algorithm:</h2>
+        <p>{description}</p>
       </div>
       <div>
         <h2>Process Queue:</h2>
         <ul>
-          {processes.map(process => (
-            <li key={process.id}>{process.name} {process.burstTime}</li>
+          {processes.map((process) => (
+            <li key={process.id}>
+              {process.name} Burst Time:
+              <input
+                type="number"
+                value={process.burstTime}
+                onChange={(e) =>
+                  handleBurstTimeChange(
+                    process.id,
+                    parseInt(e.target.value, 10)
+                  )
+                }
+              />
+            </li>
           ))}
         </ul>
       </div>
