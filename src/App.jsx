adc2c1da1bb3d1b1dea8cf493ca2aa2e32f035eb
Neecs <nikosgil12@gmail.com>
2023-11-09 18:20:13 -0500

    Revert "Merge branch 'PPA-5-Crear-round-robin-en-javaScrib'"
    This reverts commit 0b60a4f75286b43b640a1060e2b67bd42f3d761b, reversing
    changes made to 9c50b0c387c215522ca925e6215201cc16eda45d.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
-import RoundRobin from "./components/RoundRobin";
+import { Routes, Route } from "react-router-dom";
+import { ShortestJobFirst } from "./components/ShortestJobFirst/ShortestJobFirst";
+import Home from "./components/Home/Home.jsx";
+import Fifo from "./components/Fifo/Fifo.jsx";
+import RoundRobin from "./components/RoundRobin/RoundRobin.jsx";
+import {RoundRobinF} from "./components/RoundRobin/RoundRobinF.jsx";
+import Priority from "./components/Priority/Priority.jsx";
 
-const App = () => {
-    const [cola, setCola] = useState([1, 2, 3, 4, 5]);
 
-    return (
-        <div>
-            <RoundRobin cola={cola} />
-        </div>
-    );
-};
+function App() {
+  return (
+    <Routes>
+        <Route path={"/"} element={<Home/>}></Route>
+        <Route path="/sjf" element={<ShortestJobFirst />} />
+        <Route path={"/home"} element={<Home/>}></Route>
+        <Route path={"/fifo"} element={<Fifo/>}></Route>
+        <Route path={"/round-robin"} element={<RoundRobin/>}></Route>
+        <Route path={"/priority"} element={<Priority/>}></Route>
+    </Routes>
+  );
+}
 
 export default App;
