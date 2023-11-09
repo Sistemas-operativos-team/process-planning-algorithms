fe49b9cf866a3f49bd084ee907dff1f8eabc91c0
DavidSan <84092328+DavidSan9896@users.noreply.github.com>
2023-11-09 15:58:52 -0500

    Mejora

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useState } from "react";
 import RoundRobin from "./components/RoundRobin";
 
@@ -10,5 +11,26 @@ const App = () => {
         </div>
     );
 };
+=======
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ShortestJobFirst from "./components/ShortestJobFirst/ShortestJobFirst";
+import Home from "./components/Home/Home";
+import Fifo from "./components/Fifo/Fifo";
+import RoundRobin from "./components/RoundRobin/RoundRobin.jsx";
+import Priority from "./components/Priority/Priority";
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sjf" element={<ShortestJobFirst />} />
+            <Route path="/fifo" element={<Fifo />} />
+            <Route path="/round-robin" element={<RoundRobin />} />
+            <Route path="/priority" element={<Priority />} />
+        </Routes>
+    );
+}
+>>>>>>> Stashed changes
 
 export default App;
